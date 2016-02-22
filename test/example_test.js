a4c8847ce24172f6b01078ac394ecfddc7d73515
Darren Coxall <darren@darrencoxall.com>
2016-02-22 13:01:48 +0000

    Using Promises
    Promises provide a means to chaining tasks together, similar to unix and
    pipes. The output of the previous step gets passed to the input of the
    next step.
    
    What this lets us do is flatten the callback nesting and write very
    readable code that works asynchronously.
    
    Things to remember:
    
    then() always takes a function and will convert the result to a promise

--- test/example_test.js
+++ test/example_test.js
@@ -3,28 +3,32 @@ var mns    = require("../lib/example.js"),
 
 describe("Example Tests", function() {
 
-  it("fetches the latest offer", function(done) {
-    mns.getOffer(function(offer) {
-      expect(offer).to.be.eq("MOTHER'S DAY FLOWER OFFERS - LIMITED TIME ONLY");
-      // Done has to be called within callback as it is asynchronous
-      done();
-    }, done);
+  it("fetches the latest offer", function() {
+    // Mocha supports promises simply by returning them and removing the done callback
+    // We can now chain steps onto our promises. So below we getOffer and THEN using the
+    // result we make our assertions. Mocha automatically knows when the chain has ended.
+    return mns.getOffer()
+      .then(function(offer) {
+        expect(offer).to.be.eq("MOTHER'S DAY FLOWER OFFERS - LIMITED TIME ONLY");
+      });
   });
 
-  it("submits an offer", function(done) {
-    mns.submitOffer("My Example Offer", function(result) {
-      expect(result.json.offer).to.be.eq("My Example Offer");
-      done();
-    }, done);
+  it("submits an offer", function() {
+    return mns.submitOffer("My Example Offer")
+      .then(function(result) {
+        expect(result.json.offer).to.be.eq("My Example Offer");
+      });
   });
 
-  it("can fetch an offer and then submit it", function(done) {
-    mns.getOffer(function(value) {
-      mns.submitOffer(value, function(result) {
+  it("can fetch an offer and then submit it", function() {
+    // Because both our functions return promises we can now chain them together
+    // without having to nest callbacks.
+    return mns.getOffer()    // get the offer
+      .then(mns.submitOffer) // submit the result of the previous step
+      .then(function(result) {
+        // Assert the result of the previous step
         expect(result.json.offer).to.be.eq("MOTHER'S DAY FLOWER OFFERS - LIMITED TIME ONLY");
-        done();
-      }, done);
-    }, done);
+      });
   });
 
 });
