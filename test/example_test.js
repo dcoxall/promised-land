d99a1c35a85c074c1957a800d40b11e62307732b
Darren Coxall <darren@darrencoxall.com>
2016-02-22 13:13:49 +0000

    Converting arguments
    Promises become even more re-usable when you defensively convert
    arguments to promises too. This may look like too much initially but it
    makes your code more flexible as you can now use values directly or
    provided promises that will later resolve the the right value.
    
    I find the best way to think about this is that your code no longer
    needs to _know_ the values but it just needs to understand how to get
    the values.
    
    This is more of an advanced step but it makes it easier to reuse your
    functions and promises in places that you initially hadn't thought they
    would be used.

--- test/example_test.js
+++ test/example_test.js
@@ -4,9 +4,6 @@ var mns    = require("../lib/example.js"),
 describe("Example Tests", function() {
 
   it("fetches the latest offer", function() {
-    // Mocha supports promises simply by returning them and removing the done callback
-    // We can now chain steps onto our promises. So below we getOffer and THEN using the
-    // result we make our assertions. Mocha automatically knows when the chain has ended.
     return mns.getOffer()
       .then(function(offer) {
         expect(offer).to.be.eq("MOTHER'S DAY FLOWER OFFERS - LIMITED TIME ONLY");
@@ -21,12 +18,8 @@ describe("Example Tests", function() {
   });
 
   it("can fetch an offer and then submit it", function() {
-    // Because both our functions return promises we can now chain them together
-    // without having to nest callbacks.
-    return mns.getOffer()    // get the offer
-      .then(mns.submitOffer) // submit the result of the previous step
+    return mns.submitOffer(mns.getOffer())
       .then(function(result) {
-        // Assert the result of the previous step
         expect(result.json.offer).to.be.eq("MOTHER'S DAY FLOWER OFFERS - LIMITED TIME ONLY");
       });
   });
